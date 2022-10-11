a8b5949da66a3022250df8888c371549776ebfa0
gharjare <gharjarechetan@gmail.com>
2022-10-11 10:27:27 +0530

    program file

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter the year");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0) && (year % 100! == 0 || (year % 400 == 0)))
+            {
+                Console.WriteLine(year + "is leaf year");
+
+
+            }
+            else
+            {
+                Console.WriteLine(year + "is not leaf year");
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
